f96df3ce4e7f202c4afbc8c062e841b5cb9ce4e5
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-01-26 15:13:19 +0100

    inlagd flowtimer och kalender påbörjad

--- productivity-system/src/App.jsx
+++ productivity-system/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FlowTimer from "./components/FlowTimer/FlowTimer";
+import Calendar from "./components/Calendar/Calendar";
 
 export default function App() {
   return (
     <main>
       <FlowTimer />
+      <Calendar />
     </main>
   );
 }
