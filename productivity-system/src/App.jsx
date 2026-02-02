53b8f989af5e4b89770999cdc281e07c7c2be906
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-02-02 11:21:09 +0100

    uppdaterade App.jsx

--- productivity-system/src/App.jsx
+++ productivity-system/src/App.jsx
@@ -1,12 +1,46 @@
 import "./App.css";
+
+
+
+
+import Header from './components/Layout/Header';
 import FlowTimer from "./components/FlowTimer/FlowTimer";
 import Calendar from "./components/Calendar/Calendar";
+import RoutineSection from './components/RoutineSection/RoutineSection';
+import TodoWidget from './components/ToDo/TodoWidget';
+import DailySchedule from './components/Schedule/DailySchedule';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <main>
-      <FlowTimer />
-      <Calendar />
-    </main>
+    <div>
+      <Header />
+
+      <main className="dashboard-container">
+        <p>Här är dina dagliga mål och rutiner.</p>
+
+        {/* Här laddar vi in din nya komponent */}
+
+        <div className="grid-layout">
+          <div className="calendar-wrapper">
+            <Calendar />
+          </div>
+          <div className="flowtimer-wrapper">
+            <FlowTimer />
+          </div>
+          <div className="schedule-wrapper">
+            <DailySchedule />
+          </div>
+          <div className="todo-wrapper">
+            <TodoWidget />
+          </div>
+        </div>
+
+        <RoutineSection />
+
+      </main>
+    </div>
   );
 }
+
+export default App;
