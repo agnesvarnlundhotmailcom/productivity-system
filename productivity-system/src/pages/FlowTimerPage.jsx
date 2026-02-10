5d7dab285115f1efea02b748ebfadfd83c248502
FiorellaDiGiulio <fiorella.digiulio@chasacademy.se>
2026-02-10 15:19:56 +0100

    lägg till Energy nivå och statitics i flowtimer

--- productivity-system/src/pages/FlowTimerPage.jsx
+++ productivity-system/src/pages/FlowTimerPage.jsx
@@ -1,2 +1,19 @@
+import React from "react";
 import FlowTimer from "../components/FlowTimer/FlowTimer";
-export default function FlowTimerPage() { return <FlowTimer />; }
+import EnergyCare from "../components/EnergyCare/EnergyCare";
+
+export default function FlowTimerPage() {
+  return (
+    <div className="flow-page-container">
+      {/* Sektion 1: Timern */}
+      <section className="timer-section">
+        <FlowTimer />
+      </section>
+
+      {/* Sektion 2: Energy Care */}
+      <section className="energy-section" style={{ marginTop: '40px' }}>
+        <EnergyCare />
+      </section>
+    </div>
+  );
+}
