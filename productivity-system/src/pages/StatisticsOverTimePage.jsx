3f64170cbbaad260dc17c70dc654b44f8575dbcf
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-02-17 20:16:41 +0100

    skapade mappen Analys och flyttade StatisticsOverTime filerna dit

--- productivity-system/src/pages/StatisticsOverTimePage.jsx
+++ productivity-system/src/pages/StatisticsOverTimePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import StatisticsOverTime from "../components/Statistics/StatisticsOverTime";
+import StatisticsOverTime from "../components/Analys/StatisticsOverTime";
 
 export default function StatisticsOverTimePage() {
   return <StatisticsOverTime />;
