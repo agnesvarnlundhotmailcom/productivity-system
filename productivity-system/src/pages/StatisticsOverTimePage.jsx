7361e056c55802d5df747c58efffdd08e482cf4f
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-02-17 16:21:05 +0100

    nu fungerar routing till nya sidor

---
+++ productivity-system/src/pages/StatisticsOverTimePage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import StatisticsOverTime from "../components/Statistics/StatisticsOverTime";
+
+export default function StatisticsOverTimePage() {
+  return <StatisticsOverTime />;
+}
