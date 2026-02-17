f9c84e98923f9b4b970f7a12f4c0e5223c888f5f
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-02-17 14:59:16 +0100

    uppdaterade main.jsx och skapade ny page och filer

--- productivity-system/src/pages/CalendarPage.jsx
+++ productivity-system/src/pages/CalendarPage.jsx
@@ -1,39 +1,15 @@
-import React from "react";
-import Calendar from "../components/Calendar/Calendar";
-import DailySchedule from "../components/Schedule/DailySchedule";
-import TodoWidget from "../components/ToDo/TodoWidget";
+import React from 'react';
 
-export default function CalendarPage({ selectedDate, setSelectedDate }) {
-  // 1. Säkerställ att vi har ett giltigt Date-objekt för visning
-  const safeDate = selectedDate instanceof Date ? selectedDate : new Date(selectedDate);
+const StatisticsOverTime = () => {
+  const data = [
+    { date: '2024-01-01', value: 10 },
+    { date: '2024-02-01', value: 20 },
+    { date: '2024-03-01', value: 15 },
+    { date: '2024-04-01', value: 25 },
+    { date: '2024-05-01', value: 30 },
+  ];
 
-  // 2. Hantera klick från kalendern (omvandla timestamp till Date-objekt)
-  const handleDateChange = (newVal) => {
-    setSelectedDate(new Date(newVal));
-  };
+return (
 
-  return (
-    <div className="calendar-page-content">
-
-      {/* Kalendern överst */}
-      <div className="calendar-wrapper">
-        <Calendar 
-          selectedTs={safeDate} 
-          onDateChange={handleDateChange} 
-        />
-      </div>
-
-      {/* Schema och To-Do i en grid */}
-      <div className="grid-layout">
-        <div className="schedule-wrapper">
-          <DailySchedule selectedDate={safeDate} />
-        </div>
-
-        <div className="todo-wrapper">
-          <TodoWidget />
-        </div>
-      </div>
-
-    </div>
-  );
-}
+<h2>Statistics Over Time {StatisticsOverTime}</h2>
+)};
