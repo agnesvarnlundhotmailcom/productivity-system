6cbddfb54d29b138d9d08a2c5354be6715f58c09
agnes.varnlund@hotmail.com <agnes.varnlund@hotmail.com>
2026-02-23 16:11:58 +0100

    lagt in statistik

--- productivity-system/src/pages/StatisticsOverTimePage.jsx
+++ productivity-system/src/pages/StatisticsOverTimePage.jsx
@@ -1,6 +1,20 @@
 import React from "react";
-import StatisticsOverTime from "../components/Analys/StatisticsOverTime";
+import EnergyCare from "../components/EnergyCare/EnergyCare";
 
-export default function StatisticsOverTimePage() {
-  return <StatisticsOverTime />;
-}
+const StatisticsOverTime = () => {
+  return (
+    <div className="statistics-page" style={{ padding: '20px' }}>
+
+      <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+        {/* Här renderas din energimätare som hämtar data från SessionContext */}
+        <EnergyCare />
+      </div>
+      
+      <div className="additional-stats" style={{ marginTop: '40px' }}>
+        {/* Här kan du senare lägga in tabeller eller andra grafer */}
+      </div>
+    </div>
+  );
+};
+
+export default StatisticsOverTime;
