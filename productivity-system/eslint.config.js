56b0849fe588a360878153aaf85ad77a054e4158
FiorellaDiGiulio <fiorella.digiulio@chasacademy.se>
2026-03-02 15:42:42 +0100

    lyckades test

--- productivity-system/eslint.config.js
+++ productivity-system/eslint.config.js
@@ -2,10 +2,13 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jestPlugin from 'eslint-plugin-jest'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -26,4 +29,17 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+
+  {
+    files: ['**/*.test.{js,jsx}'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+
 ])
