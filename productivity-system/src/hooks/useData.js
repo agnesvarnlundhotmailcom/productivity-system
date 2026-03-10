c519f9eafd8aea062423aa260e53784b17c45d80
FiorellaDiGiulio <fiorella.digiulio@chasacademy.se>
2026-03-10 13:08:13 +0100

    hooks jsdoc

--- productivity-system/src/hooks/useData.js
+++ productivity-system/src/hooks/useData.js
@@ -1,6 +1,18 @@
 import { useContext } from 'react';
 import { DataContext } from '../contexts/DataContext';
 
+/**
+ * Hook för att få åtkomst till DataContext.
+ *
+ * @returns {Object} DataContext-objektet som innehåller delad datatillstånd och funktioner.
+ * @throws {Error} Kastas om hook:en används utanför en DataProvider.
+ *
+ * @example
+ * function MyComponent() {
+ *   const data = useData();
+ *   return <div>{data.value}</div>;
+ * }
+ */
 export function useData() {
   const context = useContext(DataContext);
   if (context === undefined) {
